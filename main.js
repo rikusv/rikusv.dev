e3a08c0809a36754435a4a8f6004b00deaf661a7
rikusv <rikusv@gmail.com>
2022-03-15 22:51:32 +0200

    Add clients and LinkedIn link

--- main.js
+++ main.js
@@ -18,96 +18,140 @@ var colors = [
   'violet'
 ]
 
-var phrases = [
+var things = [
+  {
+    value: 'GitOps',
+    size: 6
+  },
+  {
+    value: 'DevOps',
+    size: 6
+  },
   {
     value: 'TypeScript',
     size: 8
-  }, {
+  },
+  {
     value: 'JavaScript',
-    size: 8
-  }, {
+    size: 4
+  },
+  {
     value: 'Python',
     size: 8
-  }, {
-    value: 'ABAP',
-    size: 2
-  }, {
-    value: 'Java',
-    size: 2
-  }, {
+  },
+	{
     value: 'Angular',
-    size: 8
-  }, {
-    value: 'SAP UI5',
-    size: 7
-  }, {
-    value: 'React',
-    size: 3
-  }, {
-    value: 'Jekyll',
-    size: 3
-  }, {
-    value: 'Bootstrap',
     size: 4
-  }, {
-    value: 'Material Design',
-    size: 3
-  }, {
-    value: 'HTML',
-    size: 3
-  }, {
-    value: 'CSS',
-    size: 3
-  }, {
-    value: 'Node.js',
-    size: 8
-  }, {
-    value: 'SAP Business Suite',
-    size: 3
-  }, {
-    value: 'SAP HANA',
-    size: 3
-  }, {
-    value: 'SAP Gateway OData',
-    size: 3
-  }, {
-    value: 'Cloud Firestore',
-    size: 6
-  }, {
+  },
+	{
+    value: 'OpenUI5',
+    size: 2
+  },
+	{
+    value: 'React',
+    size: 2
+  },
+	{
+    value: 'Django',
+    size: 2
+  },
+	{
     value: 'PostgreSQL',
-    size: 6
-  }, {
+    size: 2
+  },
+	{
     value: 'MongoDB',
-    size: 6
-  }, {
-    value: 'SAP Cloud Platform',
-    size: 3
-  }, {
-    value: 'Google Cloud Platform',
-    size: 7
-  }, {
-    value: 'Amazon Web Services',
-    size: 7
-  }, {
-    value: 'Git',
-    size: 5
-  }, {
-    value: 'Docker',
-    size: 5
-  }, {
-    value: 'Kubernetes',
-    size: 7
-  }, {
+    size: 4
+  },
+	{
+    value: 'GCP',
+    size: 4
+  },
+	{
+    value: 'AWS',
+    size: 8
+  },
+	{
     value: 'Scuba Diving',
-    size: 5
-  }, {
+    size: 1
+  },
+	{
     value: 'Music',
-    size: 5
-  }, {
+    size: 1
+  },
+	{
     value: 'Mathematics',
-    size: 7
+    size: 1
+  },
+	{
+    value: 'Machine Learning',
+    size: 1
   }
 ]
+
+var clients = [
+  {
+    value: 'BMW',
+    size: 8,
+    url: "https://en.wikipedia.org/wiki/BMW"
+  },
+  {
+    value: 'BHP',
+    size: 2,
+    url: "https://en.wikipedia.org/wiki/BHP"
+  },
+  {
+    value: 'Coles Supermarkets',
+    size: 2,
+    url: "https://en.wikipedia.org/wiki/Coles_Supermarkets"
+  },
+  {
+    value: 'Fairfax Media',
+    size: 2,
+    url: "https://en.wikipedia.org/wiki/Fairfax_Media"
+  },
+  {
+    value: 'Insurance Australia Group',
+    size: 2,
+    url: "https://en.wikipedia.org/wiki/Insurance_Australia_Group"
+  },
+  {
+    value: 'LeasePlan',
+    size: 4,
+    url: "https://en.wikipedia.org/wiki/LeasePlan"
+  },
+  {
+    value: 'Orica',
+    size: 4,
+    url: "https://en.wikipedia.org/wiki/Orica"
+  },
+  {
+    value: 'Sanitarium',
+    size: 2,
+    url: "https://en.wikipedia.org/wiki/Sanitarium_Health_and_Wellbeing_Company"
+  },
+  {
+    value: 'SAP',
+    size: 8,
+    url: "https://en.wikipedia.org/wiki/SAP"
+  },
+  {
+    value: 'Telstra',
+    size: 6,
+    url: "https://en.wikipedia.org/wiki/Telstra"
+  },
+  {
+    value: 'UGL Limited',
+    size: 6,
+    url: "https://en.wikipedia.org/wiki/UGL_Limited"
+  },
+  {
+    value: 'Weir Group',
+    size: 6,
+    url: "https://en.wikipedia.org/wiki/Weir_Group"
+  }
+]
+
 
 function random(min, max) {
   return min + Math.floor(Math.random() * (max - min + 1))
@@ -123,18 +167,38 @@ function shuffle(array) {
   }
 }
 
-function write(array) {
+function show(array) {
+  const fontSizeDivisor = 2
+  const minFontSize = 1
+  shuffle(array)
+  document.querySelector('.phrase-container').innerHTML = null
   array.forEach(function(phrase) {
     var span = document.createElement('span')
     span.innerHTML = phrase.value
-    span.style.fontSize = Math.max(phrase.size - 5, 1) + 'em'
+    span.style.fontSize = Math.max(phrase.size / fontSizeDivisor, minFontSize) + 'em'
     span.style.color = colors[random(0, colors.length - 1)]
-    if (phrase.size < 5) {
-      span.style.opacity = phrase.size * 2 / 10
+    if (phrase.size < fontSizeDivisor * minFontSize) {
+      span.style.opacity = phrase.size / (fontSizeDivisor * minFontSize)
+    }
+    if (phrase.hasOwnProperty('url')) {
+      var link = document.createElement('a')
+      link.href = phrase.url
+      link.target = '_blank'
+      link.appendChild(span)
+      span = link
     }
     document.querySelector('.phrase-container').appendChild(span)
   })
 }
 
-shuffle(phrases)
-write(phrases)
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#thingsButton').addEventListener('click', function(evt) {
+    evt.preventDefault()
+    show(things)
+  })
+  document.querySelector('#clientsButton').addEventListener('click', function(evt) {
+    evt.preventDefault()
+    show(clients)
+  })
+  show(things)
+})
